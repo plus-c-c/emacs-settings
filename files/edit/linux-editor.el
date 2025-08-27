fb842768d7f5c87e9c00df2b5f1ebce9141e6954
Chen Jiacheng <3559084904@qq.com>
2025-08-27 14:33:42 +0800

    remove blink-search

--- files/edit/linux-editor.el
+++ files/edit/linux-editor.el
@@ -1,14 +1,3 @@
-(use-package blink-search :commands blink-search
-    :load-path "~/.emacs.d/site-lisp/blink-search"
-    :ensure posframe
-    :bind ("C-c b" . blink-search)
-    :custom
-    (blink-search-search-backends '())
-    (blink-search-enable-posframe nil)
-    (blink-search-browser-function 'eaf-open-browser)
-    (blink-search-grep-pdf-backend 'eaf-pdf-viewer)
-    )
-
 (use-package auto-save :load-path "~/.emacs.d/site-lisp/auto-save"
   :custom
   (auto-save-idle 10)
