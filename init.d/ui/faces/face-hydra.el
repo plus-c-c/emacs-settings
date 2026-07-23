6b4b84359e95aeffbf316be8d6e9ce7ba2b776aa
Chen Jiacheng <3559084904@qq.com>
2026-07-23 23:37:49 +0800

    feat: UI - face definitions for hydras and indicators

---
+++ init.d/ui/faces/face-hydra.el
@@ -0,0 +1,7 @@
+;;; face-hydra.el --- Hydra face definitions -*- lexical-binding: t -*-
+
+(face-spec-set 'hydra-face-red
+
+         '((t (:foreground "#FFa07a" :bold t))))
+
+(provide 'face-hydra)
