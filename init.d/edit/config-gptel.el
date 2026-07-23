b9aa6e7520c521d9d04d673a93b1d077c85c3f00
Chen Jiacheng <3559084904@qq.com>
2026-07-23 23:39:52 +0800

    feat: edit - GPTel AI (Ollama + DeepSeek), pasteex-mode

---
+++ init.d/edit/config-gptel.el
@@ -0,0 +1,52 @@
+;;; config-gptel.el --- GPTel AI assistant -*- lexical-binding: t -*-
+
+(use-package gptel
+
+  :ensure t
+  :commands (gptel gptel-send gptel-rewrite)
+  :bind ("C-c g" . gptel-menu)
+  ("C-c G" . gptel)
+
+  :custom
+  (gptel-default-mode 'org-mode)
+  (gptel-directives
+  '((default     . "")
+    (programming . "You are a large language model and a careful programmer. Provide code and only code as output without any additional text, prompt or note.")
+    (writing     . "You are a large language model and a writing assistant. Respond concisely.")
+    (chat        . "You are a large language model and a conversation partner. Respond concisely.")))
+  :config
+  (setq gptel-expert-commands t
+  ollama-backend-stream
+  (gptel-make-ollama "Ollama-stream"
+    :models '((deepseek-r1
+         :description "Deepseek R1"
+         :capabilities (text)
+         )
+        (gemma3
+         :description "Gemma 3"
+         :capabilities (text media)
+         ))
+    :stream t
+    )
+  ollama-backend
+  (gptel-make-ollama "Ollama"
+    :models '((deepseek-r1
+         :description "Deepseek R1"
+         :capabilities (text)
+         )
+        (gemma3
+         :description "Gemma 3"
+         :capabilities (text media)
+         ))
+          ;:stream t
+    )
+  )
+
+  (setq deepseek-backend (gptel-make-deepseek "Deepseek"
+     :stream t
+     :key (lambda () (auth-source-pick-first-password :host "api.deepseek.com"))  ; 替换为你的实际API Key
+     ))
+  (setq-default gptel-backend ollama-backend-stream)
+  (setq-default gptel-model 'deepseek-r1)
+  )
+(provide 'config-gptel)
